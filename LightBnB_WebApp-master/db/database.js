65d7a6d5bc75768caa81c9229f084fbf29bc243e
Michael Gauthier <mikegauthier222@hotmail.com>
2024-01-25 08:58:35 -0800

    fix error with searching between costs

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -145,7 +145,7 @@ const getAllProperties = (options, limit = 10) => {
 
   if (options.minimum_price_per_night && options.maximum_price_per_night) {
     queryParams.push(options.minimum_price_per_night * 100, options.maximum_price_per_night * 100);
-    queryString += `AND cost_per_night >= $${queryParams.length} AND cost_per_night <= $${queryParams.length}`;
+    queryString += `AND cost_per_night >= $${queryParams.length -1} AND cost_per_night <= $${queryParams.length}`;
   } else if (options.minimum_price_per_night) {
     queryParams.push(options.minimum_price_per_night * 100);
     queryString += `AND cost_per_night >= $${queryParams.length}`;
